1bf338decf458f664589fb17d09d54044d24dcc5
Arya Shah <aryashah0607@gmail.com>
2022-09-25 18:15:05 -0400

    part 2 done

--- test.py
+++ test.py
@@ -1,7 +1,11 @@
 import cv2 as cv
 import numpy as np
+import vision
 
-img = cv.imread('day02.jpg')
+im = cv.imread('images/day05.jpg')
+img = vision.classifyFoliage(im)
+# left top, right top, right bottom, left bottom
 c = np.array([[1960, 295], [2069, 287], [1942, 2088], [1860, 2085]])
-img2 = cv.drawContours(img, [c], 0, 255,-1)
+img2 = cv.drawContours(img, [c], 0, 128, -1)  # B, G, R
+print(img2.shape)
 cv.imwrite('bla.png', img2)
--- vision.py
+++ vision.py
@@ -1,11 +1,14 @@
+import copy
 import numpy as np
 import cv2
 from cv_utils import *
 from filterColor import createMask, transformFromBGR
 
-
+cv = cv2
 # Detect the plants in the image. Return a mask (black/white mask, where 0
 # indicates no plants and 255 indicates plants)
+
+
 def classifyFoliage(image):
     foliage_mask = np.zeros(image.shape[0:2], np.uint8)
     # Your code goes here:
@@ -14,28 +17,57 @@ def classifyFoliage(image):
     # BEGIN STUDENT CODE
     hsvImage = transformFromBGR(image, 'HSV')
     rVals = [(22, 70), (92, 255), (24, 150)]
-    mask = createMask(hsvImage, rVals, 'HSV')
-    foliage_mask = mask
+    foliage_mask = createMask(hsvImage, rVals, 'HSV')
+    print(foliage_mask.shape)
+    print(foliage_mask)
+    return foliage_mask  # 2464 * 3280, y * x
     # END STUDENT CODE
-    return foliage_mask
 
-
-# Given the foliage mask (as would be returned from classifyFoliage), 
-#   return the height in cms the tallest plant that crosses in front 
+# Given the foliage mask (as would be returned from classifyFoliage),
+#   return the height in cms the tallest plant that crosses in front
 #   of the measuring stick. Return None if no foliage overlaps the stick
+
+
 def measureHeight(foliage_mask):
     height = None
     # Your code goes here:
     # Find the maximum height of plants that overlap the measuring stick
     #   in the foliage_mask
     # BEGIN STUDENT CODE
+    c = np.array([[1970, 295], [2000, 287], [1920, 2088], [1890, 2085]])
+    color = 128
+    (m, n) = foliage_mask.shape
+    fm = copy.deepcopy(foliage_mask)
+    blue_stick = cv.drawContours(
+        foliage_mask, [c], 0, color, -1)  # B, G, R
+    heightMap = {9: 350,
+                 8: 585,
+                 7: 813,
+                 6: 1010,
+                 5: 1206,
+                 4: 1375,
+                 3: 1550,
+                 2: 1714,
+                 1: 1860,
+                 0: 2001,
+                 }
+    for i in range(m):
+        for j in range(n):
+            hm = heightMap
+            if blue_stick[i, j] == color and fm[i, j] == 255:
+                for h in range(9, 0, -1):
+                    if i <= hm[h - 1]:
+                        return (h - 1) + ((i - hm[h - 1]) / (hm[h] - hm[h - 1]))
+                return 9
     # END STUDENT CODE
-    return height
+    return None
 
 # Use the color calibration squares to find a transformation that will
 #   color-correct the image such that the mean values of the calibration
 #   squares are the given "goal" colors.
 # Return the color-corrected image
+
+
 def colorCorrect(image, blue_goal, green_goal, red_goal):
     # Your code goes here:
     # Find a transform c' = T c, c is the pixel value in the image,
@@ -51,14 +83,14 @@ def colorCorrect(image, blue_goal, green_goal, red_goal):
     # Fill in the rows of the matrix, according to the notes
     # BEGIN STUDENT CODE
     # END STUDENT CODE
-    d = np.zeros((1,9))
+    d = np.zeros((1, 9))
     # Your code goes here:
-    # Fill in the d vector with the "goal" colors 
+    # Fill in the d vector with the "goal" colors
     # BEGIN STUDENT CODE
     # END STUDENT CODE
 
     x = np.matmul(np.matmul(np.linalg.pinv(np.matmul(A.T, A)), A.T), d.T)
-    T = x.reshape((3,3))
+    T = x.reshape((3, 3))
 
     corrected_image = image.copy()
     # Your code goes here:
@@ -68,11 +100,12 @@ def colorCorrect(image, blue_goal, green_goal, red_goal):
 
     return corrected_image
 
+
 def classifyFoliageCorrected(image):
     # You can use these as "ground truth" or substitute your own
-    blue_goal =  [150, 75, 75]
+    blue_goal = [150, 75, 75]
     green_goal = [75, 150, 75]
-    red_goal =   [75, 75, 150]
+    red_goal = [75, 75, 150]
 
     # colorCorrect the image and then use classifyFoliage to
     corrected_image = colorCorrect(image, blue_goal, green_goal, red_goal)

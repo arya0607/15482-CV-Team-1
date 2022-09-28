c079a4a5515cb1635a07d157a39c6f6fb5114811
nathan-marquez <nquezt@gmail.com>
2022-09-28 10:58:50 -0400

    [CV-HW] Part 5: Add camera_behavior to LayeredAgent

--- greenhouse_agent.py
+++ greenhouse_agent.py
@@ -1,29 +1,36 @@
-import rospy, ros_hardware, layers
-import sys, os
+import rospy
+import ros_hardware
+import layers
+import sys
+import os
 from terrabot_utils import time_since_midnight
 
 import greenhouse_behaviors as gb
 import ping_behavior as ping
+import camera_behavior as camera_behavior
+
 
 class BehavioralGreenhouseAgent:
 
     def __init__(self, sim):
         if sim:
             rospy.set_param('use_sim_time', True)
-        rospy.init_node('greenhouseagent_behavioral', anonymous = True)
+        rospy.init_node('greenhouseagent_behavioral', anonymous=True)
 
         # Initialize ROSSensors, ROSActuators, BehavioralLayer, and behaviors
-         # BEGIN STUDENT CODE
-        sensors =  ros_hardware.ROSSensors()
+        # BEGIN STUDENT CODE
+        sensors = ros_hardware.ROSSensors()
         actuators = ros_hardware.ROSActuators()
-        behaviors = [gb.Light(),gb.RaiseTemp(),gb.LowerTemp(),gb.LowerHumid(),gb.RaiseSMoist(),gb.LowerSMoist(),ping.Ping()]
-        self.behavioral = layers.BehavioralLayer(sensors,actuators,behaviors)
+        behaviors = [gb.Light(), gb.RaiseTemp(), gb.LowerTemp(
+        ), gb.LowerHumid(), gb.RaiseSMoist(), gb.LowerSMoist(), ping.Ping()]
+        self.behavioral = layers.BehavioralLayer(sensors, actuators, behaviors)
         self.behavioral.startAll()
         # END STUDENT CODE
 
     def main(self):
         rospy.sleep(2)
-        while rospy.get_time() == 0: rospy.sleep(1)
+        while rospy.get_time() == 0:
+            rospy.sleep(1)
         while not rospy.core.is_shutdown():
             # Run a step of the behavioral architecture
             self.behavioral.doStep()
@@ -32,19 +39,20 @@ class BehavioralGreenhouseAgent:
 
 class LayeredGreenhouseAgent:
 
-    def __init__(self,sim,schedulefile):
+    def __init__(self, sim, schedulefile):
         if sim:
             rospy.set_param('use_sim_time', True)
-        rospy.init_node('greenhouseagent_layered', anonymous = True)
+        rospy.init_node('greenhouseagent_layered', anonymous=True)
 
         # Initialize the architecture
         # BEGIN STUDENT CODE
-        sensors =  ros_hardware.ROSSensors()
+        sensors = ros_hardware.ROSSensors()
         actuators = ros_hardware.ROSActuators()
-        behaviors = [gb.Light(),gb.RaiseTemp(),gb.LowerTemp(),gb.LowerHumid(),gb.RaiseSMoist(),gb.LowerSMoist(),ping.Ping()]
-        self.behavioral = layers.BehavioralLayer(sensors,actuators,behaviors)
+        behaviors = [gb.Light(), gb.RaiseTemp(), gb.LowerTemp(
+        ), gb.LowerHumid(), gb.RaiseSMoist(), gb.LowerSMoist(), ping.Ping(), camera_behavior.TakeImage()]
+        self.behavioral = layers.BehavioralLayer(sensors, actuators, behaviors)
         self.planning = layers.PlanningLayer(schedulefile)
-        
+
         self.executive = layers.ExecutiveLayer()
         self.planning.setExecutive(self.executive)
         self.executive.setPlanningLayer(self.planning)
@@ -55,7 +63,8 @@ class LayeredGreenhouseAgent:
 
     def main(self):
         rospy.sleep(2)
-        while rospy.get_time() == 0: rospy.sleep(1)
+        while rospy.get_time() == 0:
+            rospy.sleep(1)
         while not rospy.core.is_shutdown():
             t = time_since_midnight(rospy.get_time())
             # Run a step of each layer of the architecture
@@ -65,6 +74,7 @@ class LayeredGreenhouseAgent:
             self.executive.doStep(t)
             # END STUDENT CODE
             rospy.sleep(1)
+
 
 if __name__ == '__main__':
     sim = False
@@ -76,5 +86,5 @@ if __name__ == '__main__':
         agent.main()
     elif "-L" in sys.argv:
         print("Starting Layered Agent")
-        agent = LayeredGreenhouseAgent(sim,"greenhouse_schedule.txt")
+        agent = LayeredGreenhouseAgent(sim, "greenhouse_schedule.txt")
         agent.main()
